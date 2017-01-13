6033ae79984e7a8c6c361069104e0b0b41a3472e
Danillo Pereira <pk.danillo@gmail.com>
2017-01-13 20:27:08 -0200

    Adding collision detecter algorithm. The algorithm is based on the Kruskal Algorithm.

--- apps/collision_detecter/lib/collision_detecter.ex
+++ apps/collision_detecter/lib/collision_detecter.ex
@@ -1,2 +1,33 @@
 defmodule CollisionDetecter do
+  def detect(graph) do
+    graph
+    |> Enum.sort
+    |> detect_graph_collision
+  end
+
+  def detect_graph_collision(graph) do
+    graph
+    |> Enum.reduce([], fn(node, acc) -> find_and_insert_node(node, acc) end)
+  end
+
+  def find_and_insert_node(node, acc) do
+    node_match = fn (subset) -> Enum.any?(subset, fn(x) -> Enum.at(node, 0) == x || Enum.at(node,1) == x end) end
+    subset =  Enum.find(acc, fn (subset) -> node_match.(subset) end)
+    if subset do
+      new_subset =
+         subset
+         |> Stream.concat(node)
+         |> Enum.sort
+         |> Enum.dedup
+
+      acc
+      |> List.delete(subset)
+      |> Enum.into([new_subset])
+      |> Enum.sort
+    else
+      acc
+      |> Enum.into([node])
+      |> Enum.sort
+    end
+  end
 end
--- apps/collision_detecter/test/collision_detecter_test.exs
+++ apps/collision_detecter/test/collision_detecter_test.exs
@@ -2,7 +2,29 @@ defmodule CollisionDetecterTest do
   use ExUnit.Case
   doctest CollisionDetecter
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "return network collisions" do
+    graph = [[1,2], [2,3], [1,4], [5,6], [6, 7]]
+    assert CollisionDetecter.detect(graph) ==  [[1, 2, 3, 4], [5, 6, 7]]
+  end
+
+  test ".find_and_insert_node - doesnt find an existing node so insert a new one" do
+    node = [1,2]
+    acc = []
+    assert CollisionDetecter.find_and_insert_node(node, acc) == [[1,2]]
+  end
+
+  test ".find_and_insert_node - find an existing node and then merge the nodes" do
+    node = [2,3]
+    acc = [[1,2]]
+    assert CollisionDetecter.find_and_insert_node(node, acc) == [[1,2,3]]
+  end
+
+  test ".find_and_insert_node - find an existing three items node and then merge the nodes" do
+    acc = [[1,2]]
+    node = [2,3]
+    acc = CollisionDetecter.find_and_insert_node(node, acc)
+
+    node = [1,4]
+    assert CollisionDetecter.find_and_insert_node(node, acc) == [[1,2,3,4]]
   end
 end
