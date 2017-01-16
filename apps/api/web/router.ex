444617f4dbc0b2357ebdd74f6bdf0d6623e5afcd
Danillo Pereira <pk.danillo@gmail.com>
2017-01-16 13:39:36 -0200

    Removing unnecessary brunch and others node modules.

--- apps/api/web/router.ex
+++ apps/api/web/router.ex
@@ -1,14 +1,6 @@
 defmodule Api.Router do
   use Api.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
